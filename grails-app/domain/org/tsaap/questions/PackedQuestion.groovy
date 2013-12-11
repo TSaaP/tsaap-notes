9c963549ca3481e607018483deb3197e2b65c5fe
FranckSilvestre <franck.silvestre@ticetime.com>
2013-12-11 15:13:51 +0100

    #28 improving tests on EC question parsing (code coverage)

--- grails-app/domain/org/tsaap/questions/PackedQuestion.groovy
+++ grails-app/domain/org/tsaap/questions/PackedQuestion.groovy
@@ -5,12 +5,10 @@ class PackedQuestion {
   int questionType = 1
   String inText
 
-  String title
 
 
   static constraints = {
     questionType inList: QuestionType.values()*.code
-    title nullable: true
     inText blank: false
   }
 
--- grails-app/domain/org/tsaap/questions/UserAnswer.groovy
+++ grails-app/domain/org/tsaap/questions/UserAnswer.groovy
@@ -3,7 +3,6 @@ package org.tsaap.questions
 class UserAnswer {
 
   UserResult userResult
-  Answer answer
 
   static belongsTo = [userResult:UserResult]
 

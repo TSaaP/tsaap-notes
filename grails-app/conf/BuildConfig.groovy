18b1cbda7fb060533e0747ebb9d7a19c62e2c221
Franck Silvestre <franck.silvestre@ticetime.com>
2014-01-10 16:52:35 +0100

    change the name of the built artefact in "tsaap-notes.war"

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -17,6 +17,7 @@
 grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
+grails.project.war.file = "target/${appName}.war"
 
 
 grails.project.dependency.resolver = "maven"  // or ivy

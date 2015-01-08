c81115d5767ec36eb6acc17ad1c97a253fa68d30
Franck Silvestre <franck.silvestre@ticetime.com>
2015-01-08 15:20:21 +0100

    #58 First implementation Algorithm to select explanations to be evaluated by a user

--- grails-app/services/org/tsaap/ia/conflict/SocioCognitiveConflictService.groovy
+++ grails-app/services/org/tsaap/ia/conflict/SocioCognitiveConflictService.groovy
@@ -1,11 +1,13 @@
 package org.tsaap.ia.conflict
 
+import org.tsaap.notes.Note
 import org.tsaap.questions.LiveSessionResponse
 
 
 class SocioCognitiveConflictService {
 
     static transactional = false
+    public static final int MIN_SIZE_OF_EXPLANATION_TO_BE_EVALUATED = 10
 
     /**
      * Find the best response that is in conflict with a given response
@@ -31,5 +33,51 @@ class SocioCognitiveConflictService {
             return []
         }
         [response]
+    }
+
+    /**
+     * Get the map matching user with explanation to evaluate with round robin algorithm
+     * @param responseList the list of responses containing users and explanation to evaluate
+     * @return the map
+     */
+    Map<Long, List<Long>> explanationIdListByUserId(List<LiveSessionResponse> responseList) {
+        // get the list of suerId shuffleised
+        List<Long> userIdList = Collections.shuffle(responseList*.userId)
+        // get the notes to evaluate : only those with 100% credit
+        List<Long> explanationList = []
+        responseList.each { LiveSessionResponse response ->
+            if (response.percentCredit == 100 && response.explanation?.content?.size()> MIN_SIZE_OF_EXPLANATION_TO_BE_EVALUATED) {
+                explanationList << response.explanationId
+            }
+        }
+        // matching algorythm
+        explanationIdListByUserIdRoundRobinAlgorithm(explanationList, userIdList)
+    }
+
+    /**
+     * Get the map matching user with explanation to evaluate with round robin algorithm
+     * @param userIdList the list of user id (the keys)
+     * @param explanationList the list of explanation to distribute among all users
+     * @return the map
+     */
+    public Map<Long, List<Long>> explanationIdListByUserIdRoundRobinAlgorithm(List<Long> userIdList,List<Long> explanationList) {
+        Map<Long, List<Long>> res = [:]
+        def explanationCount = explanationList.size()
+        if (explanationCount < 4) { // all users are mapped with the all list
+            userIdList.each { Long userId ->
+                res.put(userId, explanationList)
+            }
+        } else { // apply round robin algorithm
+            int indexExplanation = 0
+            userIdList.each { Long userId ->
+                List<Long> explIdList = []
+                for (int i = 0; i < 3; i++) {
+                    explIdList << explanationList.get(indexExplanation % explanationCount)
+                    indexExplanation++
+                }
+                res.put(userId, explIdList)
+            }
+        }
+        res
     }
 }
--- test/unit/org/tsaap/ia/conflict/SocioCognitiveConflictServiceSpec.groovy
+++ test/unit/org/tsaap/ia/conflict/SocioCognitiveConflictServiceSpec.groovy
@@ -9,12 +9,47 @@ import spock.lang.Specification
 @TestFor(SocioCognitiveConflictService)
 class SocioCognitiveConflictServiceSpec extends Specification {
 
+    SocioCognitiveConflictService socioCognitiveConflictService
+
     def setup() {
+        socioCognitiveConflictService = new SocioCognitiveConflictService()
     }
 
     def cleanup() {
     }
 
-    void "test something"() {
+    void "test round robin algorithm for distribution of explanation to evaluate"() {
+        given: "a list of user Id"
+        def userIds = [1,2,3,4,5,6]
+
+        and: "a short list of explanation ids (3 or less elements)"
+        def explIds = [10,11,12]
+
+        when: "building the map"
+        Map<Long, List<Long>> res = socioCognitiveConflictService.explanationIdListByUserIdRoundRobinAlgorithm(userIds,explIds)
+
+        then: "all users are mapped with all explanations"
+        res.keySet().each {
+            println "$it, ${it.class.name}"
+        }
+        res.get(1l) == [10,11,12]
+        res.get(2l) == [10,11,12]
+        res.get(3l) == [10,11,12]
+        res.size() == 6
+        res.get(5l) == [10,11,12]
+
+        when: "the list of explanation ids has more than 3 elements"
+        explIds = [10,11,12,13]
+
+        and: "building the map"
+        res = socioCognitiveConflictService.explanationIdListByUserIdRoundRobinAlgorithm(userIds,explIds)
+
+        then: "the map si built with round robin algorithm to affect 3 explanations by user"
+        res.get(1l) == [10,11,12]
+        res.get(2l) == [13,10,11]
+        res.get(3l) == [12,13,10]
+        res.size() == 6
+        res.get(5l) == [10,11,12]
+
     }
 }

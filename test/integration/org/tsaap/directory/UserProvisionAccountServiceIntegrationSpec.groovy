850dbf85515874c161f29304a09ef44db25b9f28
Dylan <roletto.dylan@gmail.com>
2015-06-25 15:56:24 +0200

    upgrade UserProvisionAccountService code coverage

--- test/integration/org/tsaap/directory/UserProvisionAccountServiceIntegrationSpec.groovy
+++ test/integration/org/tsaap/directory/UserProvisionAccountServiceIntegrationSpec.groovy
@@ -28,11 +28,14 @@ class UserProvisionAccountServiceIntegrationSpec extends Specification{
         def username = userProvisionAccountService.generateUsername(sql,"john","doe")
         lmsUserHelper.insertUserInDatabase(sql,'jdoe@mail.com','john','doe','jdoe','pass')
         def username2 = userProvisionAccountService.generateUsername(sql,'jane','doe')
+        lmsUserHelper.insertUserInDatabase(sql,'jane@mail.com','jane','doe','jdoe2','passw')
+        def username3 = userProvisionAccountService.generateUsername(sql,'jean','doel')
+
 
         then: "I got a different username for the two users"
         username == "jdoe"
         username2 == "jdoe2"
-
+        username3  == "jdoe3"
 
     }
 }

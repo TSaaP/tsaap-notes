92dbc16621ad2e3cb813d8525150fdfafe3ae83f
Abdelmoghite Kacimi <moghitkacimi@gmail.com>
2016-05-25 11:29:11 +0200

    Update notificationService to use unsubscribeKey

--- grails-app/services/org/tsaap/notes/NotificationService.groovy
+++ grails-app/services/org/tsaap/notes/NotificationService.groovy
@@ -28,11 +28,7 @@ class NotificationService {
   def notifyUsersOnTodayNotes() {
     Map notifications = findAllNotifications()
     notifications.each { user, contextList ->
-      def u = User.findById(user.user_id)
-      key = UnsubscribeKey.findByUser(u)
-      if (!key) {
-        key = unsubscribeKeyService.createKeyForUser(u)
-      }
+
 
       try {
         def sub = messageSource.getMessage("email.notes.notification.title",null,new Locale(user.language))
@@ -40,7 +36,7 @@ class NotificationService {
           to user.email
           subject sub
           html view: "/email/notesNotification", model: [user: user,
-                  contextList: contextList, key:key.unsubscribeKey]
+                  contextList: contextList, key:contextList.key]
         }
       } catch (Exception e) {
         log.error("Error with ${user.email} : ${e.message}")
@@ -60,24 +56,26 @@ class NotificationService {
   private Map findAllNotifications() {
     def sql = new Sql(dataSource)
     def req = """
-              SELECT tuser.id as user_id, tuser.first_name, tuser.email, tuser.language, tcontext.id as context_id, tcontext.context_name, count(tnote.id) as count_notes
+              SELECT tuser.id as user_id, tuser.first_name, tuser.email, tuser.language, tcontext.id as context_id, tcontext.context_name, count(tnote.id) as count_notes, tkey.unsubscribe_key as ukey
               FROM note as tnote
               INNER JOIN context_follower as tcontextfo ON tnote.context_id = tcontextfo.context_id
               INNER JOIN context as tcontext ON tcontextfo.context_id = tcontext.id
               INNER JOIN user as tuser ON tcontextfo.follower_id = tuser.id
               INNER JOIN settings as tsettings ON tsettings.user_id = tuser.id
+              INNER JOIN unsubscribe_key as tkey ON tkey.user_id = tuser.id
               where tnote.date_created < NOW() and tnote.date_created > concat(date_sub(curdate(),interval 1 day),' ',curtime()) and tuser.enabled = TRUE
                 and tsettings.daily_notifications = 1
-              group by context_id, user_id
+              group by context_id, user_id, tkey.id
               UNION
-              SELECT tuser.id as user_id, tuser.first_name, tuser.email, tuser.language, tcontext.id as context_id, tcontext.context_name, count(tnote.id)
+              SELECT tuser.id as user_id, tuser.first_name, tuser.email, tuser.language, tcontext.id as context_id, tcontext.context_name, count(tnote.id), tkey.unsubscribe_key as ukey
               FROM note as tnote
               INNER JOIN context as tcontext ON tnote.context_id = tcontext.id
               INNER JOIN user as tuser ON tcontext.owner_id = tuser.id
               INNER JOIN settings as tsettings ON tsettings.user_id = tuser.id
+              INNER JOIN unsubscribe_key as tkey ON tkey.user_id = tuser.id
               where tnote.date_created < NOW() and tnote.date_created > concat(date_sub(curdate(),interval 1 day),' ',curtime()) and tuser.enabled = TRUE
                 and tsettings.daily_notifications = 1
-              group by context_id, user_id
+              group by context_id, user_id, tkey.id
               order by user_id,context_name """
     def rows = sql.rows(req)
     def notifications = [:]
@@ -86,7 +84,7 @@ class NotificationService {
       if (notifications[key] == null) {
         notifications[key] = []
       }
-      notifications[key] << [context_id: it.context_id, context_name: it.context_name, count_notes: it.count_notes]
+      notifications[key] << [context_id: it.context_id, context_name: it.context_name, count_notes: it.count_notes, key: it.ukey]
     }
     sql.close()
     notifications

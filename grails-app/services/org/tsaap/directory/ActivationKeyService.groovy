227d2234eaa155bc412da06ec922b011b1e6fa66
Abdelmoghite Kacimi <moghitkacimi@gmail.com>
2016-05-27 10:05:33 +0200

    Update test for removing old activation keys

--- grails-app/services/org/tsaap/directory/ActivationKeyService.groovy
+++ grails-app/services/org/tsaap/directory/ActivationKeyService.groovy
@@ -10,7 +10,15 @@ class ActivationKeyService {
         def lifetime = grailsApplication.config.tsaap.auth.activation_key.lifetime_in_hours ?: 3
         def keys = ActivationKey.findAllByDateCreatedLessThan(DateUtils.addHours(new Date(), -lifetime))
         ActivationKey.deleteAll(keys)
-        UserRole.executeUpdate('''
+        def users = User.findAllByIdInListAndEnabled(keys.user.id, false)
+        def settings = Settings.findAllByUserInList(users)
+        def roles = UserRole.findAllByUserInList(users)
+
+        UserRole.deleteAll(roles)
+        Settings.deleteAll(settings)
+        User.deleteAll(users)
+
+        /*UserRole.executeUpdate('''
                 delete from UserRole ur
                 where ur.user in (
                     select u.id from User u
@@ -27,6 +35,6 @@ class ActivationKeyService {
         User.executeUpdate('''
                 delete from User u
                 where u.enabled = false and u.id not in (select ak.user from ActivationKey ak)
-                ''')
+                ''')*/
     }
 }

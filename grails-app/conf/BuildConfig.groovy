a74979a6f51ce9bad653c707f690b20ab7494e80
Franck Silvestre <franck.silvestre@ticetime.com>
2016-08-26 18:05:46 +0200

    try to fix travis build by changing httpclient library

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -62,9 +62,9 @@ grails.project.dependency.resolution = {
         compile 'com.github.TSaaP:tsaap-lti:0.2'
 
         // Latest httpcore and httpmime for Coveralls plugin
-        build 'org.apache.httpcomponents:httpcore:4.3.2'
-        build 'org.apache.httpcomponents:httpclient:4.3.2'
-        build 'org.apache.httpcomponents:httpmime:4.3.3'
+        build 'org.apache.httpcomponents:httpcore:4.4.5'
+        build 'org.apache.httpcomponents:httpclient:4.5.2'
+        build 'org.apache.httpcomponents:httpmime:4.5.2'
     }
 
     plugins {
